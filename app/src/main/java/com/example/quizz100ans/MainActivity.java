b383d0845ae70706438d163b714702f22c558949
test2slam@jolsio.net <thomaslaroche4444@gmail.com>
2019-12-10 17:56:47 +0000

    Sauvegarde d'une première interface

--- app/src/main/java/com/example/quizz100ans/MainActivity.java
+++ app/src/main/java/com/example/quizz100ans/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.quizz100ans;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.quizz100ans.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(com.example.quizz100ans.R.layout.activity_main);
+
     }
 }
